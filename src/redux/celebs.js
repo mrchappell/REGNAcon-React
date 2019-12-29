8c77b08f3b5e2612e4d1f593cad4181fbd6a40e7
mrchappell <michael.r.chappell@gmail.com>
2019-12-28 19:58:30 -0600

    combined reducers

---
+++ src/redux/celebs.js
@@ -0,0 +1,8 @@
+import { CELEBS } from '../shared/celebs';
+
+export const Celebs = (state = CELEBS, action) => {
+    switch (action.type) {
+        default:
+          return state;
+      }
+};
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,18 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import {createStore, combineReducers} from 'redux';
+import { Celebs } from './celebs';
+import { Comments } from './comments';
+import { Partners } from './partners';
+import { Promotions } from './promotions';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            celebs: Celebs,
+            comments: Comments,
+            partners: Partners,
+            promotions: Promotions
+        })
     );
 
     return store;
-};
+}
